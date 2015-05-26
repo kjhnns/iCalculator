c66b5cedb9a83243e39d6255a76b040611f6fd9d
Johannes Klumpe <klumpe@live.de>
2015-05-26 23:17:23 +0200

    Fehler ausgemerzt

--- _quiz.php
+++ _quiz.php
@@ -1,27 +1,27 @@
 <?php
 $questions = array(
     array(
-          "q" => "125<sup>4</sup> + 24 × 1999"
+          "q" => "125<sup>4</sup> + 24 × 1999",
           "a" => "244188601"
           ),
     array(
-          "q" => "125<sup>3</sup> + 24 × 1999"
+          "q" => "125<sup>3</sup> + 24 × 1999",
           "a" => "2001101"
           ),
     array(
-          "q" => "125<sup>2</sup> + 24 × 1999"
+          "q" => "125<sup>2</sup> + 24 × 1999",
           "a" => "63601"
           ),
     array(
-          "q" => "125 + 24"
+          "q" => "125 + 24",
           "a" => "149"
           ),
     array(
-          "q" => "12 + 24"
+          "q" => "12 + 24",
           "a" => "36"
           ),
     array(
-          "q" => "1 + 1"
+          "q" => "1 + 1",
           "a" => "2"
           )
 );
@@ -41,7 +41,7 @@ $q = array_rand($questions);
                </section>
             <h5>Aktuelles Taschenrechner Ergebnis:</h5>
                <p><div id="result" style="text-align: center; font-weight: bold">0</div></p>
-               <p style="text-align: center;"><input type="button" value="Prüfen" id="check" data-value="<?=$q['a']?>" /></p>
+               <p style="text-align: center;"><input type="button" value="Prüfen" id="check" data-value="<?=$q["a"]?>" /></p>
             </section>
          </div>
          <div class="4u -1u">
