b88eb3e883e458dbc6d1f3415232a75bd4fde1cf
Johannes Klumpe <klumpe@live.de>
2015-05-26 22:33:50 +0200

    initial

---
+++ template/lib/init.js
@@ -0,0 +1,90 @@
+/*
+	Ion by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+
+(function($) {
+
+	skel.init({
+		reset: 'full',
+		breakpoints: {
+
+			// Global.
+				global: {
+					range: '*',
+					href: 'template/stylesheets/style.css',
+					containers: 1024,
+					grid: {
+						gutters: {
+							vertical: '4em',
+							horizontal: 0
+						}
+					}
+				},
+
+			// XLarge.
+				xlarge: {
+					range: '-1680',
+					href: 'template/stylesheets/style-xlarge.css',
+					containers: 1024
+				},
+
+			// // Large.
+			// 	large: {
+			// 		range: '-1280',
+			// 		href: 'template/stylesheets/style-large.css',
+			// 		containers: 960,
+			// 		grid: {
+			// 			gutters: {
+			// 				vertical: '2.5em'
+			// 			}
+			// 		},
+			// 		viewport: {
+			// 			scalable: false
+			// 		}
+			// 	},
+
+			// // Medium.
+			// 	medium: {
+			// 		range: '-980',
+			// 		href: 'template/stylesheets/style-medium.css',
+			// 		containers: '90%',
+			// 		grid: {
+			// 			collapse: 1
+			// 		}
+			// 	},
+
+			// // Small.
+			// 	small: {
+			// 		range: '-736',
+			// 		href: 'template/stylesheets/style-small.css',
+			// 		containers: '90%',
+			// 		grid: {
+			// 			gutters: {
+			// 				vertical: '1.25em'
+			// 			}
+			// 		}
+			// 	},
+
+			// // XSmall.
+			// 	xsmall: {
+			// 		range: '-480',
+			// 		href: 'template/stylesheets/style-xsmall.css',
+			// 		grid: {
+			// 			collapse: 2
+			// 		}
+			// 	}
+
+		},
+		plugins: {
+		}
+	});
+
+	$(function() {
+
+		// jQuery ready stuff.
+
+	});
+
+})(jQuery);
